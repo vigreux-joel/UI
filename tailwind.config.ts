bac567220b4a1fbfa262a4e569b584f8dc8a0362
Joël <vigreux.joel@hotmail.com>
2023-12-04 08:00:33 +0100

    Refactor tailwind.config.ts to use createConfig method
    This commit refactors tailwind.config.ts to utilize the createConfig method from the tailwind-material package, deprecating the use of createMaterialTheme. This change simplifies the theme creation logic and moves the primary color definition into the config creation. The refactor enhances code readability and maintainability, aligning theme creation with new function signatures.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,28 @@
 import type { Config } from 'tailwindcss';
-import { createMaterialTheme } from '../tailwind-material/src';
+import { createConfig } from '../tailwind-material/src';
 
-const materialTailwind = createMaterialTheme({
-  colors: {
-    palette: {
+const config: Config = createConfig(
+  {
+    darkMode: 'class',
+    content: [
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+      './stories/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
+      },
+    },
+  },
+  {
+    colorPalette: {
       primary: '#6750A4',
     },
-  },
-  darkMode: 'class',
-});
+  }
+);
 
-const config: Config = {
-  darkMode: 'class',
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './stories/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    colors: materialTailwind.colors,
-    fontFamily: materialTailwind.fontFamily,
-
-    extend: {
-      boxShadow: {
-        '1': '0px 1px 3px 1px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)',
-        '2': '0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)',
-        '3': '0px 1px 3px 0px rgba(0, 0, 0, 0.30), 0px 4px 8px 3px rgba(0, 0, 0, 0.15)',
-        '4': '0px 2px 3px 0px rgba(0, 0, 0, 0.30), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)',
-        '5': '0px 4px 4px 0px rgba(0, 0, 0, 0.30), 0px 8px 12px 6px rgba(0, 0, 0, 0.15)',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [...materialTailwind.plugins],
-};
 export default config;
